d1efe1767f101043491b284ddd0c21ccc0dee5d9
meteor <tjdans34568@gmail.com>
2023-02-25 16:41:27 +0900

    Initial commit

---
+++ src/main/java/dev/study/springscurity2/SpringScurity2Application.java
@@ -0,0 +1,13 @@
+package dev.study.springscurity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScurity2Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/study/springscurity2/SpringScurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.study.springscurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScurity2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
